a1d38903bdab23ef0c9f9bc170905dbc84348660
VanyaMate <buhu653@mail.ru>
2023-06-14 11:00:44 +0300

    add passthrought

--- src/modules/api/auth/auth.controller.ts
+++ src/modules/api/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Res, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, Post, Res, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { CreateUserDto } from "../../user/dto/create-user.dto";
 import { ClassValidatorPipe } from "../../../pipes/class-validator.pipe";
@@ -6,7 +6,7 @@ import { COOKIE_ACCESS_TOKEN } from "../../../constants/cookies.constant";
 import { Response } from 'express';
 import { getMsDays } from "../../../helpers/utils.helper";
 import { AccessTokenGuard } from "../../../guards/access-token.guard";
-import { ApiCookieAuth, ApiHeader, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { ApiHeader, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import {
     LogoutSwagger,
     ResponseUnauthorizedSwagger,
@@ -14,7 +14,6 @@ import {
     UserSwagger
 } from "../../../configs/swagger.config";
 import { IUserVerifiedData, UserVerified } from "../../../decorators/user-verified.decorator";
-import { UserPrivate } from "../../user/user.interface";
 
 @ApiTags('Аутентификация')
 @Controller('/api/auth')
@@ -26,15 +25,16 @@ export class AuthController {
     @ApiResponse({ status: 200, type: UserSwagger })
     @ApiResponse({ status: 401, type: ResponseUnauthorizedSwagger })
     @Post('/login')
+    @HttpCode(200)
     @UsePipes(ClassValidatorPipe)
     async login (@Body() createUserDto: CreateUserDto,
-                 @Res() res: Response) {
+                 @Res({ passthrough: true }) res: Response) {
         const { user, jwtToken } = await this.authService.login(createUserDto);
         res.cookie(COOKIE_ACCESS_TOKEN, jwtToken, {
             httpOnly: true,
             maxAge: getMsDays(30)
         })
-        res.status(200).json(user);
+        return user;
     }
 
     @ApiOperation({ summary: 'Регистрация по логину и паролю' })
@@ -42,13 +42,14 @@ export class AuthController {
     @Post('/registration')
     @UsePipes(ClassValidatorPipe)
     async registration (@Body() createUserDto: CreateUserDto,
-                        @Res() res: Response) {
+                        @Res({ passthrough: true }) res: Response) {
         const { user, jwtToken } = await this.authService.registration(createUserDto);
         res.cookie(COOKIE_ACCESS_TOKEN, jwtToken, {
             httpOnly: true,
             maxAge: getMsDays(30)
         })
-        res.status(200).json(user);
+
+        return user;
     }
 
     @ApiOperation({ summary: 'Выход' })
@@ -57,17 +58,16 @@ export class AuthController {
     @ApiHeader({ name: COOKIE_ACCESS_TOKEN, description: 'Cookie.httpOnly jwt token', example: SWAGGER_JWT_EXAMPLE })
     @Get('/logout')
     @UseGuards(AccessTokenGuard)
-    logout (@Res() res: Response) {
+    logout (@Res({ passthrough: true }) res: Response) {
         res.clearCookie(COOKIE_ACCESS_TOKEN);
-        res.status(200).json({ logout: true })
+        return { logout: true };
     }
 
     @Get('/refresh')
     @UseGuards(AccessTokenGuard)
-    async refresh (@Res() res: Response,
-           @UserVerified() user: IUserVerifiedData) {
-        const userData: UserPrivate = await this.authService.refresh(user.id)
-        res.status(200).json(userData)
+    async refresh (@Res({ passthrough: true }) res: Response,
+                   @UserVerified() user: IUserVerifiedData) {
+        return await this.authService.refresh(user.id);
     }
 
 
