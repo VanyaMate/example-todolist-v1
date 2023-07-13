8c241d9b3bf578b9138663261f756dc83a04af51
VanyaMate <buhu653@mail.ru>
2023-07-13 22:07:57 +0300

    fix

--- src/modules/database/sequelize/database-sequelize.providers.ts
+++ src/modules/database/sequelize/database-sequelize.providers.ts
@@ -10,15 +10,18 @@ export const databaseProviders = [
     {
         provide: 'SEQUELIZE',
         useFactory: async (config: ConfigService) => {
-            const sequelize = new Sequelize({
-                dialect: config.get<Dialect>('DB_SEQUELIZE_DIALECT'),
-                host: config.get<string>('DB_SEQUELIZE_HOST'),
-                port: Number(config.get<number>('DB_SEQUELIZE_PORT') ?? 3000),
-                username: config.get<string>('DB_SEQUELIZE_USERNAME'),
-                password: config.get<string>('DB_SEQUELIZE_PASS'),
-                database: config.get<string>('DB_SEQUELIZE_DATABASE'),
-                logging: false,
-            });
+            // TODO: Fix this fix for render.com
+            const sequelize = process.env.NODE_ENV === 'prod'
+                ?   new Sequelize('postgres://vanyamate:d4DGaej1OOkA4jH4dv4ZJ3N6yMZWmtmG@dpg-cio46ctgkuvudi8mar5g-a.frankfurt-postgres.render.com/todolist_ytqw')
+                :   new Sequelize({
+                        dialect: config.get<Dialect>('DB_SEQUELIZE_DIALECT'),
+                        host: config.get<string>('DB_SEQUELIZE_HOST'),
+                        port: Number(config.get<number>('DB_SEQUELIZE_PORT') ?? 3000),
+                        username: config.get<string>('DB_SEQUELIZE_USERNAME'),
+                        password: config.get<string>('DB_SEQUELIZE_PASS'),
+                        database: config.get<string>('DB_SEQUELIZE_DATABASE'),
+                        logging: false,
+                    });
             sequelize.addModels([User, Token, TodoItem, TodoList]);
             await sequelize.sync();
             return sequelize;
